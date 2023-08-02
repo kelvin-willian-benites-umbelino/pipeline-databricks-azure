185bc416432d72789ad1afc51773b2bafa4eb7f8
kelvin-willian-benites-umbelino <kelvin-willian-benites-umbelino>
2023-08-02 14:01:14 +0000

    spark

--- notebooks/Coleta_servicenow_bronze.scala
+++ notebooks/Coleta_servicenow_bronze.scala
@@ -1,24 +1,23 @@
 // Databricks notebook source
 // MAGIC %python
-// MAGIC from datetime import datetime
 // MAGIC from datetime import date, timedelta
-// MAGIC import sys
+// MAGIC from pyspark.sql import SparkSession
+// MAGIC from pyspark.sql.functions import col
 // MAGIC
-// MAGIC import requests
-// MAGIC from pandas import json_normalize
-// MAGIC import numpy as np
-// MAGIC import pandas as pd
-// MAGIC import os
-// MAGIC import requests
-// MAGIC import pandas as pd
-// MAGIC import time
+// MAGIC data_df_inicio = date.today() - timedelta(days=10)
+// MAGIC data_df_fim = date.today() - timedelta(days=1)
 // MAGIC
-// MAGIC data_df_inicio = date.today - timedelta(days=1)
-// MAGIC
+// MAGIC # Inicialize a sessão do Spark
+// MAGIC spark = SparkSession.builder.appName("Exemplo_PySpark").getOrCreate()
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC # Configurações da API
 // MAGIC sysparm_table = 'sn_customerservice_case_dga'
 // MAGIC sysparm_fields = 'number, u_krotonpad_treeview'
 // MAGIC sysparm_limit = 10000
-// MAGIC sysparm_query = f"""sys_created_byLIKE-ate^sys_created_onBETWEENjavascript:gs.dateGenerate('{data_df_inicio}','00:00:00')@javascript:gs.dateGenerate('{data_df_inicio}','00:59:59')^u_canal_entradaLIKETelefone"""       
+// MAGIC sysparm_query = f"""sys_created_byLIKE-ate^sys_created_onBETWEENjavascript:gs.dateGenerate('{data_df_inicio}','00:00:00')@javascript:gs.dateGenerate('{data_df_fim}','00:59:59')^u_canal_entradaLIKETelefone"""
 // MAGIC
 // MAGIC username = 'lucas.mg'
 // MAGIC password = '123'
@@ -31,10 +30,27 @@
 // MAGIC     "sysparm_limit": sysparm_limit,
 // MAGIC     "sysparm_exclude_reference_link": "true",
 // MAGIC     "sysparm_suppress_pagination_header": "true"
-// MAGIC }   
+// MAGIC }
 // MAGIC
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC # Chame a API usando o módulo requests para obter os dados
+// MAGIC import requests
 // MAGIC response = requests.get(url, auth=(username, password), headers=headers, params=query_params)
 // MAGIC response.raise_for_status()
 // MAGIC data = response.json()
-// MAGIC result = pd.json_normalize(data["result"])
 // MAGIC
+// MAGIC # Converta os dados para um DataFrame do PySpark
+// MAGIC from pyspark.sql import Row
+// MAGIC data_rows = [Row(**item) for item in data["result"]]
+// MAGIC df = spark.createDataFrame(data_rows)
+// MAGIC
+// MAGIC # Exiba o DataFrame
+// MAGIC df.show()
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/bronze/service_now"
+df.write.format("delta").mode(SaveMode.Overwrite).save(path)
